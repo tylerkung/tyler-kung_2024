00c814df189d968b7efdee19df5eb87875fbb5b8
Tyler Kung <tkung12@gmail.com>
2021-08-03 11:24:51 -0700

    clean up

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,6 @@
-var football_vid = 'https://www.youtube.com/embed/vcpV7IwNp_8';
-var basketball_vid = 'https://www.youtube.com/embed/e5vFZCGBXUY';
-var lcs_vid = 'https://www.youtube.com/embed/xu_iQr3dhTk';
-var viewport;
+
 
 $(document).ready(function(){
-	$('video').each(function(){
-		$(this)[0].load();
-	});
-})
-
-$('.nav-item i').click(function(e){
-	e.preventDefault();
-	$(this).parents('.has-children').toggleClass('active');
-});
-$('.mobile-menu-btn').click(function(e){
-	$(this).toggleClass('open');
-	$('.nav').toggleClass('active');
-	$('body').toggleClass('lock-scroll');
-});
-
-$('.video-thumbnail').click(function(e){
-	var id = $(this).attr('id');
-	var currentVid;
-	if (id === 'football-tutorial') currentVid = football_vid;
-	else if (id === 'basketball-tutorial') currentVid = basketball_vid;
-	else if (id === 'lcs-tutorial') currentVid = lcs_vid;
-
-	$('#yt-player').html('<iframe width="1920" height="1080" src="' + currentVid + '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="" title="LCS Video"></iframe>');
-	$('.tutorial-video').addClass('active');
-});
-$('.video-bg').click(function(e){
-	$('.tutorial-video').removeClass('active');
-	$('#yt-player').html('');
 })
 
 var controller = new ScrollMagic.Controller({});
